350d698e2cbe6192e21bc07e653928355ed603c2
Alex Chinyavong <chinyavonga@vcu.edu>
2021-05-09 15:35:48 -0400

    update

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,17 @@
 def spiralize(size, n=1):
-    """ Your code goes somewhere in here"""
-    return_value = n
-    return return_value
+    n = 1
+    count = 0
+    gap = 2
+    total = 0
+
+    while n <= size ** 2:
+        total += n
+        n += gap
+        count += 1
+        if count == 4:
+            gap += 2
+            count = 0
+
+    return total
+    
+
